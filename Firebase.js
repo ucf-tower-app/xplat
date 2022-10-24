b162bf5c63e336ca39cc1a86aaa54419114d6f6e
Jacob S <jakesteinebronn@gmail.com>
2022-10-24 18:13:10 -0400

    no more dotenv

--- Firebase.js
+++ Firebase.js
@@ -1,23 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
-import * as dotenv from 'dotenv';
-dotenv.config()
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId,
-};
+import { firebaseConfig } from "./.firebase_config";
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
