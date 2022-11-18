7f0534bf879506a38d0029940a288027d960e70e
Jacob S <jakesteinebronn@gmail.com>
2022-11-18 17:19:39 -0500

    Add comment class

--- types/comment.ts
+++ types/comment.ts
@@ -1,13 +1,11 @@
 import { LazyObject } from "./common";
-import { DocumentReference, DocumentData, getDoc } from "firebase/firestore";
-import { Comment, User } from "./types";
+import { getDoc } from "firebase/firestore";
+import { User } from "./types";
 
-export class Post extends LazyObject {
+export class Comment extends LazyObject {
     private author: User | undefined;
     private timestamp: Date | undefined;
     private textContent: string | undefined;
-    private likes: User[] | undefined;
-    private comments: Comment[] | undefined;
 
     private async getData() {
         if (this.hasData) return;
@@ -18,8 +16,6 @@ export class Post extends LazyObject {
             this.author = new User(data.author);
             this.timestamp = data.timestamp;
             this.textContent = data.textContent;
-            this.likes = data.likes.map((ref: DocumentReference<DocumentData>) => new User(ref));
-            this.comments = data.comments.map((ref: DocumentReference<DocumentData>) => new Comment(ref));
 
             this.hasData = true;
         }
@@ -38,15 +34,5 @@ export class Post extends LazyObject {
     public async getTextContent() {
         if (!this.hasData) await this.getData();
         return this.textContent!;
-    }
-
-    public async getLikes() {
-        if (!this.hasData) await this.getData();
-        return this.likes!;
-    }
-
-    public async getComments() {
-        if (!this.hasData) await this.getData();
-        return this.comments!;
     }
 }
