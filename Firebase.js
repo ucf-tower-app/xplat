3803186cd36a92ad98e4563bf5704bbea00a57ad
Jacob S <jakesteinebronn@gmail.com>
2022-11-18 23:23:44 -0500

    Auth and emails

--- Firebase.js
+++ Firebase.js
@@ -4,6 +4,10 @@ import { getFirestore } from 'firebase/firestore';
 
 import { firebaseConfig } from "./.firebase_config";
 
+import { getAuth } from "firebase/auth";
+
+
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth();
