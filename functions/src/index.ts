5382000ad51624b3565096543afecc86ef9568b2
Jacob S <jakesteinebronn@gmail.com>
2022-11-29 19:15:54 -0500

    Comment out User onCreate because of #10

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,25 +1,27 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import * as functions from "firebase-functions";
+// import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
-import {EventContext} from "firebase-functions";
-import {DocumentSnapshot} from "firebase-functions/v1/firestore";
+// import {EventContext} from "firebase-functions";
+// import {DocumentSnapshot} from "firebase-functions/v1/firestore";
 admin.initializeApp();
-const db = admin.firestore();
+// const db = admin.firestore();
 
-exports.addUserToCache = functions.firestore.document("/users/{userId}")
-    .onCreate((snap, context: EventContext) => {
-      const username: string = snap.data().username;
-      const uid: string = context.params.userId;
+// No longer needed, but kept as an example. Replaced with client transaction.
 
-      functions.logger.log("Adding user", username, "with id", uid);
-      return db.doc("caches/users").get()
-          .then((userCacheSnap: DocumentSnapshot) => {
-            const map = userCacheSnap.data()!.usernameToUserID;
-            map[username] = uid;
-            return db.doc("caches/users").set({usernameToUserID: map},
-                {merge: true});
-          })
-          .catch((error) => {
-            functions.logger.log(error);
-          });
-    });
+// exports.addUserToCache = functions.firestore.document("/users/{userId}")
+//     .onCreate((snap, context: EventContext) => {
+//       const username: string = snap.data().username;
+//       const uid: string = context.params.userId;
+
+//       functions.logger.log("Adding user", username, "with id", uid);
+//       return db.doc("caches/users").get()
+//           .then((userCacheSnap: DocumentSnapshot) => {
+//             const map = userCacheSnap.data()!.usernameToUserID;
+//             map[username] = uid;
+//             return db.doc("caches/users").set({usernameToUserID: map},
+//                 {merge: true});
+//           })
+//           .catch((error) => {
+//             functions.logger.log(error);
+//           });
+//     });
