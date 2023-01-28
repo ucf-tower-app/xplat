775d16294d0a42ebe7876804cfb8e7bad21541a7
Jacob S <jakesteinebronn@gmail.com>
2023-01-27 19:26:31 -0500

    Remove unused arrays and unnecessary doc updates to these arrays

--- types/forum.ts
+++ types/forum.ts
@@ -1,29 +1,14 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-import {
-  DocumentData,
-  DocumentReference,
-  collection,
-  orderBy,
-  where,
-} from 'firebase/firestore';
+import { DocumentData, collection, orderBy, where } from 'firebase/firestore';
 import { db } from '../Firebase';
 import { LazyObject, Post, QueryCursor, Route } from './types';
 
 export class Forum extends LazyObject {
-  // Filled with defaults if not present when getting data
-  public posts?: Post[];
-  public _isArchived?: boolean;
-
   // Might remain undefined even if has data
   public route?: Route;
 
   public initWithDocumentData(data: DocumentData) {
-    this.posts = (data.posts ?? []).map(
-      (ref: DocumentReference<DocumentData>) => new Post(ref)
-    );
     if (data.route) this.route = new Route(data.route);
-
-    this._isArchived = data.isArchived ?? true;
 
     this.hasData = true;
   }
@@ -56,24 +41,12 @@ export class Forum extends LazyObject {
     if (!this.hasData) await this.getData();
     return this.route;
   }
-
-  /** isArchived
-   */
-  public async isArchived() {
-    if (!this.hasData) await this.getData();
-    return this._isArchived;
-  }
 }
 
 export class ForumMock extends Forum {
-  constructor(posts: Post[]) {
+  constructor() {
     super();
-    this.posts = posts;
 
     this.hasData = true;
-  }
-
-  public addPosts(posts: Post[]) {
-    this.posts = this.posts?.concat(posts);
   }
 }
