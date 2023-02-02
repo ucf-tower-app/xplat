9204483fc14dafb8bcd4a2a55c2f28e70c1ce89f
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2023-02-02 16:50:00 -0500

    Persist auth on RN

--- Firebase.ts
+++ Firebase.ts
@@ -1,13 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getAuth, initializeAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { getReactNativePersistence } from "firebase/auth/react-native"
+import AsyncStorage from "@react-native-async-storage/async-storage"
 
 import { firebaseConfig } from './.firebase_config';
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
+// Set up auth persistence on react-native
+if (typeof navigator !== 'undefined' && navigator.product === 'ReactNative') {
+  initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage)
+  })
+}
+
 export const db = getFirestore(app);
 export const auth = getAuth();
 export const storage = getStorage();
