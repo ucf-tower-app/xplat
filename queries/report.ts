fd39c30635a35de74344467d33552cb87ae91871
AlexRutledge1 <31017536+AlexRutledge1@users.noreply.github.com>
2023-02-20 16:00:52 -0500

    include proper docref data

--- queries/report.ts
+++ queries/report.ts
@@ -37,27 +37,29 @@ export function getIQParams_Reports() {
 
 export type ReportedContent = {
   content: User | Comment | Post;
-  reporters: User[];
+  reporter: User;
 }
 
-export function constructReportPageData(docs: QueryDocumentSnapshot<DocumentData>[]) {
+export function constructReportPageData(docs: QueryDocumentSnapshot<DocumentData>[]): ReportedContent[] {
   return docs.map((doc) => {
     const data = doc.data();
-    console.log(data);
-    const pathArr = data.content._key.path.segments;
-    if (pathArr.includes('posts'))
+    
+    const reporterPathArr = data.reporter._key.path.segments;
+    const reporter = new User(data.reporter);
+    const contentPathArr = data.content._key.path.segments;
+    if (contentPathArr.includes('posts'))
     {
-      const p = new Post(pathArr[pathArr.length - 1]);
-      return p; 
+      const p = new Post(data.content);
+      return {content: p, reporter: reporter} as ReportedContent; 
     }
-    if (pathArr.includes('comments'))
+    if (contentPathArr.includes('comments'))
     {
-      const c = new Comment(pathArr[pathArr.length - 1]);
-      return c;
+      const c = new Comment(data.content);
+      return {content: c, reporter: reporter} as ReportedContent;
     }
     
-    const u = new User(pathArr[pathArr.length - 1]);
-    return u;
+    const u = new User(data.content);
+    return {content: u, reporter: reporter} as ReportedContent;
     
   });
 }
