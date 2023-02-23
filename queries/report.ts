4e512d4cafc249f83912effd3b5ab81247bc6f84
AlexRutledge1 <31017536+AlexRutledge1@users.noreply.github.com>
2023-02-23 18:28:00 -0500

    better variable names

--- queries/report.ts
+++ queries/report.ts
@@ -43,23 +43,21 @@ export type ReportedContent = {
 export function constructReportPageData(docs: QueryDocumentSnapshot<DocumentData>[]): ReportedContent[] {
   return docs.map((doc) => {
     const data = doc.data();
-    
-    const reporterPathArr = data.reporter._key.path.segments;
     const reporter = new User(data.reporter);
     const contentPathArr = data.content._key.path.segments;
     if (contentPathArr.includes('posts'))
     {
-      const p = new Post(data.content);
-      return {content: p, reporter: reporter} as ReportedContent; 
+      const post = new Post(data.content);
+      return {content: post, reporter: reporter} as ReportedContent; 
     }
     if (contentPathArr.includes('comments'))
     {
-      const c = new Comment(data.content);
-      return {content: c, reporter: reporter} as ReportedContent;
+      const comment = new Comment(data.content);
+      return {content: comment, reporter: reporter} as ReportedContent;
     }
     
-    const u = new User(data.content);
-    return {content: u, reporter: reporter} as ReportedContent;
+    const user = new User(data.content);
+    return {content: user, reporter: reporter} as ReportedContent;
     
   });
 }
